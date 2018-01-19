ac1a351c9ba01fc29e17bdf69067f14117e86094
David Kuruvilla <dapawn@gmail.com>
2018-01-19 00:16:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
 
     if req.path.match(/items/)
       @@items.each do |item|
+        item ||= "Your cart is empty"
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
